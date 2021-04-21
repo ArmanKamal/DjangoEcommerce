3989e0c61c1568720038c039f5a4420eb8bb75a1
ArmanKamal <armankamal2421@gmail.com>
2021-04-21 15:19:40 -0500

    Added Dynamic Checkout Page

--- carts/views.py
+++ carts/views.py
@@ -8,7 +8,7 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        
+
     context={
         "items":items,
         'order': order
@@ -16,5 +16,15 @@ def cart(request):
     return render(request, 'carts/cart.html',context)
 
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, order_completed=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        
+    context={
+        "items":items,
+        'order': order
+    }
     return render(request, 'carts/checkout.html',context)    
