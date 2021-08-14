c8fa11d4f3aaf2ffa2d5aff19a8b76454b86b50c
ArmanKamal <armankamal2421@gmail.com>
2021-08-14 15:43:26 -0500

    debug mode on

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@)zq8&)ctz!#32szkg*7_ot$&nv6v=2(k3oh!p*-+*6lrhzy8l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://djangoshopify.herokuapp.com/','127.0.0.1']
 
